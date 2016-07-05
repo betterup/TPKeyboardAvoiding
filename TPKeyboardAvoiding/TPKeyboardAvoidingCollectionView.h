2518ad70dedee1e998ecfcfed7d21e1dc25ab2f5
Ryan Sonnek <ryan@codecrate.com>
2016-07-05 11:29:54 -0700

    revert to unmaintained version

--- TPKeyboardAvoiding/TPKeyboardAvoidingCollectionView.h
+++ TPKeyboardAvoiding/TPKeyboardAvoidingCollectionView.h
@@ -1,6 +1,5 @@
 //
 //  TPKeyboardAvoidingCollectionView.h
-//  TPKeyboardAvoiding
 //
 //  Created by Michael Tyson on 30/09/2013.
 //  Copyright 2015 A Tasty Pixel & The CocoaBots. All rights reserved.
